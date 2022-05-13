d633d95d0022fd821bbb092fbd18885bd26c19df
Yossapol Witayanont <reef_3llen@hotmail.com>
2022-05-13 17:55:38 +0700

    use fastify and build swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,61 @@
-import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import { join } from 'path';
 import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core';
+import { fastifyHelmet } from 'fastify-helmet';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+require('dotenv').config({
+  path: process.env.DOTENV_PATH || '.env',
+});
+
+const port = process.env.PORT || 9000;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+
+  await app.register(fastifyHelmet, {
+    contentSecurityPolicy: {
+      directives: {
+        defaultSrc: [`'self'`],
+        styleSrc: [`'self'`, `'unsafe-inline'`],
+        imgSrc: [`'self'`, 'data:', 'validator.swagger.io'],
+        scriptSrc: [`'self'`, `https: 'unsafe-inline'`],
+      },
+    },
+  });
+
+  app.useStaticAssets({
+    root: join(__dirname, '..', 'public'),
+    prefix: '/public/',
+  });
+  app.setViewEngine({
+    engine: {
+      handlebars: require('handlebars'),
+    },
+    templates: join(__dirname, '..', 'views'),
+  });
+
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  app.enableCors();
+
+  const config = new DocumentBuilder()
+    .setTitle('fit-to-earn-api')
+    .setDescription('Fit to Earn API')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
+
+  await app.listen(port);
 }
+
 bootstrap();
